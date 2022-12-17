8c7aef0fc7cea72ea2cd2ea4d3be0d934d4b06ad
John Costa <johncosta027@gmail.com>
2022-12-17 14:46:39 +0000

    Final changes to add authors

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const cheerio = require("cheerio");
 
 const url =
   "https://pure.royalholloway.ac.uk/en/persons/konstantinos-markantonakis/publications/?page=";
+
 const resSelector = "div.result-container > div > h3 > a";
-const citSelector = "div.tab-container > div#cite-harvard";
+const citSelector = "div.tab-container > div#cite-harvard > div.rendering";
+const authorsSelector = 'p.relations';
+const linkSelector = 'div.link > a';
+const otherLinkSelector = 'div.doi > a';
 
 async function GetPagePapers(number) {
   const page = await axios.get(url + number);
@@ -23,7 +27,31 @@ async function GetPagePapers(number) {
   return data.map((d) => {
     $ = cheerio.load(d.data);
     const text = $(citSelector).text();
-    return text;
+    const textHtml = $('div.tab-container > div#cite-harvard > div.rendering').html();
+
+    text.replace(/'/g, "");
+
+    const authors = $(authorsSelector).html();
+
+    const doi1 = $(linkSelector).attr()?.href;
+    const doi2 = $(otherLinkSelector).attr()?.href;
+    
+    const doi = doi1 === undefined ? doi2 : doi1;
+
+    const split = textHtml.split(/[0-9]{4}/);
+    split.shift();
+    split[0] = split[0].slice(2);
+
+    let returnText = split.reduce((p, n) => p + n, "");
+    const lastLink = returnText.lastIndexOf('<a onclick="window.open(this.href)');
+    if (lastLink > 2) {
+      returnText = returnText.slice(0, lastLink);
+      if (doi) {
+        returnText += `<a href="${doi}">${doi}</a>`
+      }
+    }
+
+    return authors + " " + returnText;
   });
 }
 
@@ -39,7 +67,8 @@ async function Main() {
   console.log("Total amount of papers:", totalRefs.length);
   let counter = totalRefs.length;
   for (const r of totalRefs) {
-    console.log(`${counter}: ` + r.replace('\n', '').trim());
+    console.log(`[${counter}] ` + r.replace('\n', '').trim());
+    console.log();
     counter--;
   }
 }
